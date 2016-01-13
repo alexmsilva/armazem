5f902ee6403ab16aa1c6213850729ec1778808e5
Alex Silva <alexmsilva@gmail.com>
2016-01-12 22:00:34 -0200

    lerdiando o app fazendo views com blade

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Armazém</title>
+		<link rel="stylesheet" type="text/css" href="/css/app.css">
+		<!--<script type="text/javascript" src="/js/bootstrap.min.js"></script>-->
+	</head>
+	<body>
+		@yield("conteudo")
+	</body>
+</html>
