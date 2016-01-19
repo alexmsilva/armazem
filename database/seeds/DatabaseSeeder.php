9287f94c2b5492c03c2af9fb791798cbe72bc7a5
Alex <alex.silva@batanga.com>
2016-01-19 17:07:37 -0200

    adicionando dados inciais/default no banco

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,22 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use armazem\Categoria;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$this->call('CategoriaTableSeeder');
+	}
+}
+
+class CategoriaTableSeeder extends Seeder {
+	public function run() {
+		Categoria::create(array('nome'=>'Brinquedos'));
+		Categoria::create(array('nome'=>'Esportes'));
+	}
 }
